faf72910dccaa177614e31fff0f8b5bdb8bbaec5
dapfreemanq2 <diodora_r_856210410@rambler.ru>
2025-12-05 10:52:53 +0800

    add customer-simple-payment.java, IInputVerifier.dbg.json, config-profile-test.rs

---
+++ responsive/config-profile-test.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\responsive\config-profile-test.rs
+// Generated at: 2025-10-25T13:41:18.838Z
---
+++ sms-local/customer-simple-payment.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\sms-local\customer-simple-payment.java
+// Generated at: 2025-10-25T13:41:18.848Z
